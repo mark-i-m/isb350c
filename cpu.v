2d7e8f4f04bc42073535103f28de80a6aba9aaba
Mark Mansi <mmansi@utexas.edu>
2015-05-01 22:15:04 -0500

    Added the RSs and some minor cleanup

--- cpu.v
+++ cpu.v
@@ -2,9 +2,36 @@
 
 // Simple Tomasulo processor
 
+// Lots of MACROS to make Verilog more likeable. Imagine macros making
+// a language better!
+
+// Opcodes
+`define MOV 0
+`define ADD 1
+`define JMP 2
+`define HLT 3
+`define LD  4
+`define LDR 5
+`define JEQ 6
+
+// Bit fields
 `define REG_BUSY(i) regs[i][31]
 `define REG_SRC(i)  regs[i][30:16]
 `define REG_VAL(i)  regs[i][15:0]
+
+`define RS_ISSUED(rs_num) rs[rs_num][51]
+`define RS_BUSY(rs_num) rs[rs_num][50]
+`define RS_OP(rs_num) rs[rs_num][49:46]
+`define RS_READY(rs_num, which) (!which ? rs[rs_num][45] : rs[rs_num][44])
+`define RS_SRC(rs_num, which) (!which ? rs[rs_num][43:38] : rs[rs_num][37:32])
+`define RS_VAL(rs_num, which) (!which ? rs[rs_num][31:16] : rs[rs_num][15:0])
+
+// Parameters
+`define NUM_REGS (1<<`REGS_WIDTH)
+`define REGS_WIDTH 4
+
+`define NUM_RS (1<<`RS_WIDTH)
+`define RS_WIDTH 3 // 2 ** RS_WIDTH = NUM_RS
 
 module main();
 
@@ -23,7 +50,15 @@ module main();
 
     // registers: busy(1bit), src(15bit), val(16bit)
     // TODO: update from dispatcher
-    reg regs[31:0];
+    reg [31:0]regs[`NUM_REGS-1:0];
+
+    // Init all registers to not busy
+    integer reg_init_counter;
+    initial begin
+        for (reg_init_counter = 0; reg_init_counter < `NUM_REGS; reg_init_counter = reg_init_counter + 1) begin
+            `REG_BUSY(reg_init_counter) <= 0;
+        end
+    end
 
     // memory
     wire [15:0]imem_raddr_out;
@@ -76,23 +111,10 @@ module main();
         ib_pop, ib_data_out, ib_empty,
         ib_flush);
 
+    // Dispatch
     // This is the dispatcher logic. It is mostly a big block
     // of combinational logic that was best in its own file...
     // but Verilog =[
-    //
-    // Inputs:
-    // CDB
-    // IB
-    //
-    // Outputs:
-    // ib_flush?
-    // branch_taken?/branch_target
-    // REGS
-    //  - which regs to update
-    //  - how to update them
-    // RSs
-    //  - which RSs to update
-    //  - what are the values
 
     // Decode the instructions
     wire [15:0]pc = ib_data_out[31:16];
@@ -106,13 +128,17 @@ module main();
     wire [3:0]d = inst[3:0];
 
     // various dispatcher flags
+
+    // Is the current instruction valid
     wire opcode_v = !ib_empty && !waiting_jeq && !fus_full;
 
+    // Is the dispatcher waiting for JEQ to be resolved
     reg waiting_jeq = 0;
     always @(posedge clk) begin
         waiting_jeq <= opcode_v && opcode == `JEQ && !jeqReady;
     end
 
+    // Is a pending JEQ ready?
     wire jeqReady = cdb0_op == `JEQ && cdb0_v; // TODO: undefined wires: cdb0
 
     // Dispatcher and regs
@@ -122,14 +148,36 @@ module main();
     // this is a flag: should we write to rt?
     wire rt_v = opcode_v && (opcode == `MOV || opcode == `ADD || opcode == `LD || opcode == `LDR); // TODO: hook this up
     // this is the val to write to rt
-    wire [31:0]rt_val = {1'h1, rs_num}; // TODO: rs_num
+    wire [31:0]rt_val = {1'h1, rs_num}; // TODO: undefined wire: rs_num
 
     // another flag: should we write to RS?
     wire rs_v = rt_v && opcode_v && opcode == `JEQ;
     // another value: what to write into the rs
-    wire [/* Put stuff here */]rs_val = 0 ;// TODO: insert stuff here
+    wire [51:0]rs_val = 0 ;// TODO: insert stuff here
 
     // RSs
+    // bits field in order
+    // 1    issued
+    // 1    busy
+    // 4    op
+    // 2    ready
+    // 12   src
+    // 32   val
+    // -----------
+    // 51   total
+
+    // 4 reservation stations for fxu: 0, 1, 2, 3
+    // 4 reservation stations for ld: 4, 5, 6, 7
+    reg [51:0]rs[0:`NUM_RS-1];
+
+    // Init all RSs to not busy and not issued
+    integer rsn;
+    initial begin
+        for (rsn = 0 ; rsn < `NUM_RS; rsn = rsn + 1) begin
+            `RS_ISSUED(rsn) <= 0;
+            `RS_BUSY(rsn) <= 0;
+        end
+    end
 
     // CDB
 

dfaea2f14a2924f8fe51fc9166293da3d42224f3
Mark Mansi <mmansi@utexas.edu>
2015-05-08 10:28:46 -0500

    Working on ISB

--- isb.v
+++ isb.v
@@ -10,10 +10,41 @@
 // Training unit: 4 entries
 // Steam predictor: 1 stream buffer with a queue of 4 entries, degree 1
 
+// Lots of macros:
+`define TU_V(tu_num) //TODO: define these
+`define TU_PC(tu_num)
+`define TU_LAST(tu_num)
+
 module isb(input clk,
-    input v_in, input [15:0]pc, input [15:0]addr,
+    input v_in, input [15:0]pc, input [15:0]addr, //TODO: hook these up
     output prefetch_v, output [15:0]prefetch_addr
 );
+
+// next unallocated SA
+// allocate 16 addrs at a time
+reg [31:0]next_sa = 0;
+
+// training unit
+// bits | field
+// 1    | valid
+// 16   | pc
+// 16   | last addr
+// -----|-----------
+// 33   | total
+//
+// 4 entries
+reg [32:0]tu[3:0];
+// TODO: need LRU? Use round robin?
+
+// lookup pc in the TU
+wire pc_v = (`TU_PC(0) == pc && `TU_V(0)) ||
+            (`TU_PC(1) == pc && `TU_V(1)) ||
+            (`TU_PC(2) == pc && `TU_V(2)) ||
+            (`TU_PC(3) == pc && `TU_V(3)) ;
+wire pc_last = (`TU_PC(0) == pc && `TU_V(0)) ? `TU_LAST(0) :
+               (`TU_PC(1) == pc && `TU_V(1)) ? `TU_LAST(1) :
+               (`TU_PC(2) == pc && `TU_V(2)) ? `TU_LAST(2) :
+               `TU_LAST(3);
 
 // PS-AMC
 // bits | field
@@ -27,6 +58,9 @@ module isb(input clk,
 // 32 entries
 reg [50:0]psamc[31:0];
 
+wire a = psamc_lookup(pc_last) // TODO: undefined wire
+wire b = psamc_lookup(addr) // TODO:undefined wire
+wire ab_comp = {`PSAMC_V(a), `PSAMC(b)}; // compare presence of a and b
 
 // SP-AMC
 // bits | field
@@ -40,21 +74,35 @@ reg [50:0]psamc[31:0];
 reg [288:0]spamc[1:0];
 
 
-// training unit
-// bits | field
-// 1    | valid
-// 16   | pc
-// 16   | last addr
-// -----|-----------
-// 33   | total
-//
-// 4 entries
-reg [32:0]tu[3:0];
-
-
 // stream predictor
 // fifo queue of 4 entries
 fifo #(2) sb0(/*TODO: ports*/);
 
+
+// update logic
+always @(posedge clk) begin
+    // training
+    if (pc_v && pc_last != addr) begin
+        // Update mappings
+        case(ab_comp)
+            0 : begin
+                // TODO:neither a nor b in psamc
+            end
+            1 : begin
+                // TODO:only b in psamc
+            end
+            2 : begin
+                // TODO:only a in psamc
+            end
+            3 : begin
+                // TODO:both in psamc
+            end
+        endcase
+    end else if (!pc_v) begin
+        // TODO:insert into TU
+    end
+end
+
+// TODO: prediction
 
 endmodule

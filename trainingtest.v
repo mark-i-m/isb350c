80217fa9d7ab62899991922abea825eddbdc5a6a
Mark Mansi <mmansi@utexas.edu>
2015-05-09 12:33:18 -0500

    Debugging TU

---
+++ trainingtest.v
@@ -0,0 +1,55 @@
+`timescale 1ps/1ps
+
+module ttest();
+
+    // debugging
+    initial begin
+        $dumpfile("test.vcd");
+        $dumpvars(4,ttest);
+    end
+
+    // clk
+    wire clk;
+    clock c0(clk);
+
+    // counter
+    counter ctr(stop_ctr, clk, 0, cycle);
+    reg stop_ctr = 0;
+
+    wire [15:0]cycle;
+
+    // stuff
+    reg v_in = 0;
+    reg [15:0]pc;
+    reg [15:0]addr;
+
+    isb #(1) i0(clk,
+        v_in, pc, addr,
+        ,
+    );
+
+    always @(posedge clk) begin
+        case(cycle)
+            10 : begin
+                v_in <= 1;
+                pc <= 16'h0000;
+                addr <= 16'h0010;
+            end
+            11 : begin
+                v_in <= 1;
+                pc <= 16'h0000;
+                addr <= 16'h0011;
+            end
+
+            100 : begin
+                stop_ctr <= 1;
+            end
+
+            default : begin
+                v_in <= 0;
+                pc <= 16'hxxxx;
+                addr <= 16'hxxxx;
+            end
+        endcase
+    end
+endmodule
